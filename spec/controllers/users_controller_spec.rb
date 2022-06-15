0a1e2ed1462b8989f93546cdd2ccf7d180cb6613
adityatriand <adityatriananda0@gmail.com>
2022-06-15 18:31:28 +0700

    add testing for GET #index

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe UsersController do
+    describe 'GET #index' do
+        it "populates an array of all users" do 
+            user1 = create(:user, email: "tes@gmail.com")
+            user2 = create(:user, email: "tes1@gmail.com")
+            get :index
+            expect(assigns(:users)).to match_array([user1, user2])
+        end
+
+        it "renders the :index template" do
+            get :index
+            expect(response).to render_template :index
+        end
+    end
 end
