63dab98e09423edce1d1e0df3ea9d724e4dfe845
adityatriand <adityatriananda0@gmail.com>
2022-06-15 22:36:12 +0700

    add testing for GET #new

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -34,4 +34,16 @@ RSpec.describe UsersController do
     end
   end
 
+  describe 'GET #new' do
+    it "assigns a new user to @user" do
+      get :new
+      expect(assigns(:user)).to be_a_new(User)
+    end
+
+    it "renders the :new template" do
+      get :new
+      expect(:response).to render_template :new
+    end
+  end
+
 end
