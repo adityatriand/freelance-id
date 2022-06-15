d0675cddc8bd438f87acabd4f3aafd3a6c2c5e0c
adityatriand <adityatriananda0@gmail.com>
2022-06-15 18:09:23 +0700

    add validation for email field

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,5 +10,27 @@ RSpec.describe User, type: :model do
       expect(user).to be_valid
     end
   end
-  
+
+  context 'validation for email field' do
+    it 'is invalid without email' do
+      user.email = nil
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end 
+
+    it 'is invalid if use wrong format email' do
+      user.email = "halo"
+      user.valid?
+      expect(user.errors[:email]).to include("is invalid")
+    end
+
+    it 'is invalid if use same email' do
+      user1 = FactoryBot.create(:user, email: 'tes@gmail.com')
+      user2 = FactoryBot.build(:user, email: 'tes@gmail.com')
+      user2.valid?
+      expect(user2.errors[:email]).to include("has already been taken")
+    end
+
+  end
+
 end
