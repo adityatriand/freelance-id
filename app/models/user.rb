b8863d2d6c1adbed959546f34874013d1b0c5e19
adityatriand <adityatriananda0@gmail.com>
2022-06-15 17:54:42 +0700

    fix bug hash bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
     validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
-    validates :password_digest, presence: true, length: { minimum: 8 }
+    validates :password, presence: true, length: { minimum: 8 }
     validates :role, presence: true, numericality: { only_integer: true, in: 0..2 }
 
     has_secure_password
