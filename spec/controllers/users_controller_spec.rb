0022fb8ece58487114be90ec6bf9ad9bca4b565c
adityatriand <adityatriananda0@gmail.com>
2022-06-15 22:37:02 +0700

    add testing for GET #edit

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -46,4 +46,18 @@ RSpec.describe UsersController do
     end
   end
 
+  describe 'GET #edit' do
+    it "assigns the requested user to @user" do
+      user = create(:user)
+      get :edit, params: { id: user }
+      expect(assigns(:user)).to eq user
+    end
+
+    it "renders the :edit template" do
+      user = create(:user)
+      get :edit, params: { id: user }
+      expect(response).to render_template :edit
+    end
+  end
+
 end
