7994ff058cd01106fcb4eb1946b566c955f3390b
adityatriand <adityatriananda0@gmail.com>
2022-06-19 12:10:01 +0700

    [FIX] give relation to client model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_one :client, dependent: :destroy
 
     validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
     validates :password, presence: true, length: { minimum: 8 }
